2d8f0772b2f7809209e70a6c6b2dfb03f0074227
reshmabee <reshma.bee.us@gmail.com>
2022-08-15 18:52:29 -0400

    my initial version

---
+++ src/main/java/com/example/AccountLookup/AccountLookupApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccountLookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AccountLookup/AccountLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccountLookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
