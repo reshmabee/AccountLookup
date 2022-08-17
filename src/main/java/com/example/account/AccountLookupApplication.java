c0b4b9ec145b8048ef82c289fcc93a87be7f7702
reshmabee <reshma.bee.us@gmail.com>
2022-08-17 18:09:53 -0400

    AccountLookup

--- src/main/java/com/example/account/AccountLookupApplication.java
+++ src/main/java/com/example/account/AccountLookupApplication.java
@@ -1,4 +1,4 @@
-package com.example.AccountLookup;
+package com.example.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class AccountLookupApplication {
 	}
 
 }
+
--- src/test/java/com/example/account/AccountLookupApplicationTests.java
+++ src/test/java/com/example/account/AccountLookupApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.AccountLookup;
+package com.example.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
